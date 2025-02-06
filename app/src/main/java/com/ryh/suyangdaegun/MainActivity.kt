4b6cc30acaf5c1f317cb67e3b854ddd4a26afecd
kci01 <kci01184@naver.com>
2025-02-06 12:19:47 +0900

    zzz

--- app/src/main/java/com/ryh/suyangdaegun/MainActivity.kt
+++ app/src/main/java/com/ryh/suyangdaegun/MainActivity.kt
@@ -62,30 +62,4 @@ fun MainScreen(navController: NavHostController) {
     }
 }
 
-@Composable
-fun AppNavigator(navController: NavHostController = rememberNavController()) {
-    val viewModel: RegistrationViewModel = viewModel() // RegistrationViewModel 생성
 
-    CompositionLocalProvider(LocalViewModelStoreOwner provides LocalViewModelStoreOwner.current!!) {
-        NavHost(
-            navController = navController,
-            startDestination = "login"
-        ) {
-            composable("login") { LoginScreen(navController) }
-            composable("main") { MainScreen(navController) }
-            composable("matching") { MatchingScreen(navController) }
-            composable("chatList") { ChatListScreen(navController) }
-            composable("chatting") { ChattingScreen(navController) }
-            composable("myPage") { MyPageScreen(navController) }
-            composable("faceAnalysis") { FaceAnalysisScreen(navController) }
-
-            // 회원가입 관련 경로
-            composable("gender") { GenderStep(navController, viewModel) }
-            composable("nickname") { NicknameStep(navController, viewModel) }
-            composable("birthdate") { BirthdateStep(navController, viewModel) }
-            composable("profilePicture") { ProfilePictureStep(navController, viewModel) }
-            composable("interests") { InterestsStep(navController, viewModel) }
-            composable("complete") { CompleteStep(navController, viewModel) }
-        }
-    }
-}
--- app/src/main/java/com/ryh/suyangdaegun/Navigator.kt
+++ app/src/main/java/com/ryh/suyangdaegun/Navigator.kt
@@ -11,104 +11,30 @@ import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
 
 
-//@Composable
-//fun AppNavigator() {
-//    val navController = rememberNavController()
-//    val firebaseAuth = FirebaseAuth.getInstance()
-//    val coroutineScope = rememberCoroutineScope()
-//
-//    // 로그인 상태 확인
-//    LaunchedEffect(Unit) {
-//        coroutineScope.launch {
-//            delay(1500) // 로딩 화면 대체
-//            if (firebaseAuth.currentUser != null) {
-//                navController.navigate("main") {
-//                    popUpTo("loading") { inclusive = true }
-//                }
-//            } else {
-//                navController.navigate("accession") {
-//                    popUpTo("loading") { inclusive = true }
-//                }
-//            }
-//        }
-//    }
-//
-//    NavHost(
-//        navController = navController,
-//        startDestination = "login"
-//    ) {
-//        composable("loading") { LoadingScreen() }
-//        composable("login") {
-//            LoginScreen(
-//                onGoogleLogin = { navController.navigate("accession") },
-//                onKakaoLogin = { navController.navigate("accession") },
-//                onNaverLogin = { navController.navigate("accession") }
-//            )
-//        }
-//        composable("main") {
-//            MainScreen(
-//                onNavigateToMatching = { navController.navigate("matching") },
-//                onNavigateToChatList = { navController.navigate("chatList") },
-//                onNavigateToMyPage = { navController.navigate("myPage") }
-//            )
-//        }
-//        composable("accession") {
-//            AccessionNavigator(navController = navController)
-//        }
-//        composable("matching") {
-//            MatchingScreen(onNavigateBack = { navController.popBackStack() })
-//        }
-//        composable("chatList") {
-//            ChatListScreen(onNavigateToChat = { navController.navigate("chat") })
-//        }
-//        composable("chat") {
-//            ChatScreen(viewModel = ChatViewModel())
-//        }
-//        composable("myPage") {
-//            MyPageScreen(onNavigateBack = { navController.popBackStack() })
-//        }
-//    }
-//}
+@Composable
+fun AppNavigator(navController: NavHostController = rememberNavController()) {
+    val viewModel: RegistrationViewModel = viewModel() // RegistrationViewModel 생성
 
-//@Composable
-//fun AppNavigator(navController: NavHostController, startDestination: String) {
-//    CompositionLocalProvider(LocalViewModelStoreOwner provides LocalViewModelStoreOwner.current!!) {
-//        NavHost(navController = navController, startDestination = startDestination) {
-//            composable("login") { LoginScreen(navController) }
-//            composable("accession") { AccessionNavigator(navController) }
-//            composable("main") { MainScreen(navController) }
-//        }
-//    }
-//}
+    CompositionLocalProvider(LocalViewModelStoreOwner provides LocalViewModelStoreOwner.current!!) {
+        NavHost(
+            navController = navController,
+            startDestination = "login"
+        ) {
+            composable("login") { LoginScreen(navController) }
+            composable("main") { MainScreen(navController) }
+            composable("matching") { MatchingScreen(navController) }
+            composable("chatList") { ChatListScreen(navController) }
+            composable("chatting") { ChattingScreen(navController) }
+            composable("myPage") { MyPageScreen(navController) }
+            composable("faceAnalysis") { FaceAnalysisScreen(navController) }
 
-
-
-//// 네비게이션 컨트롤러
-//@Composable
-//fun AccessionNavigator(navController: NavHostController) {
-//    val viewModel: RegistrationViewModel = viewModel()
-//
-//    NavHost(
-//        navController = navController,
-//        startDestination = "gender"
-//    ) {
-//        composable("gender") {
-//            GenderStep(navController, viewModel)
-//        }
-//        composable("nickname") {
-//            NicknameStep(navController, viewModel)
-//        }
-//        composable("birthdate") {
-//            BirthdateStep(navController, viewModel)
-//        }
-//        composable("profilePicture") {
-//            ProfilePictureStep(navController, viewModel)
-//        }
-//        composable("interests") {
-//            InterestsStep(navController, viewModel)
-//        }
-//        composable("complete") {
-//            CompleteStep(navController, viewModel)
-//        }
-//    }
-//}
+            // 회원가입 관련 경로
+            composable("gender") { GenderStep(navController, viewModel) }
+            composable("nickname") { NicknameStep(navController, viewModel) }
+            composable("birthdate") { BirthdateStep(navController, viewModel) }
+            composable("profilePicture") { ProfilePictureStep(navController, viewModel) }
+            composable("interests") { InterestsStep(navController, viewModel) }
+            composable("complete") { CompleteStep(navController, viewModel) }
+        }
+    }
+}
