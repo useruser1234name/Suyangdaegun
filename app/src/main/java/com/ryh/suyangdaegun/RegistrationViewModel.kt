a730a21d3622c974aa7a31b38ff2ba4b148729ba
kci01 <kci01184@naver.com>
2025-02-06 13:45:19 +0900

    zzz

--- app/src/main/java/com/ryh/suyangdaegun/RegistrationViewModel.kt
+++ app/src/main/java/com/ryh/suyangdaegun/RegistrationViewModel.kt
@@ -1,17 +1,25 @@
 package com.ryh.suyangdaegun
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class RegistrationViewModel : ViewModel() {
-    private var gender: String? = null
-    private var nickname: String? = null
-    private var birthdate: String? = null
-    private var profilePicture: String? = null
-    private var interests: List<String> = emptyList()
+    var gender by mutableStateOf("")
+        private set
+    var nickname by mutableStateOf("")
+        private set
+    var birthdate by mutableStateOf("")
+        private set
+    var profilePicture by mutableStateOf("")
+        private set
+    var interests = mutableStateListOf<String>()
 
     fun setGender(value: String) { gender = value }
     fun setNickname(value: String) { nickname = value }
     fun setBirthdate(value: String) { birthdate = value }
     fun setProfilePicture(value: String) { profilePicture = value }
-    fun setInterests(value: List<String>) { interests = value }
+    fun setInterests(value: List<String>) { interests.clear(); interests.addAll(value) }
 }
