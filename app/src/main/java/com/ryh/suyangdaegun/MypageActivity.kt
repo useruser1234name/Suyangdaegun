3aee348455e070c47cceeeea823e7d7e855ba2ac
kci01 <kci01184@naver.com>
2025-02-12 11:33:01 +0900

    ChattingActivity.kt UI Editing ChattingListActivity.kt UI Editing

--- app/src/main/java/com/ryh/suyangdaegun/MypageActivity.kt
+++ app/src/main/java/com/ryh/suyangdaegun/MypageActivity.kt
@@ -1,5 +1,8 @@
 package com.ryh.suyangdaegun
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.BasicTextField
@@ -9,7 +12,9 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,12 +30,146 @@ fun MyPageScreen(navController: NavHostController) {
             .verticalScroll(rememberScrollState())
             .padding(16.dp)
     ) {
-        Text("채팅리스트", fontSize = 28.sp)
-        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            "마이페이지", fontSize = 25.sp,
+            modifier = Modifier.height(50.dp)
+        )
+
         Divider(modifier = Modifier.fillMaxWidth())
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { navController.navigate("chatting/default") }) {
-            Text("채팅 화면으로 이동")
+
+
+
+        Text(
+            text = "알림",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(horizontal = 16.dp),
+            color = Color.Black
+        )
+
+        Divider(
+            color = Color.Gray, // 선의 색상
+            thickness = 0.5.dp,   // 선의 두께
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        )
+        NoticeList(
+            noticeItems = listOf(
+                "📢 공지사항",
+                "🎉 이벤트",
+                "고객센터",
+                "📝 자주 묻는 질문",
+                "1:1 문의하기"
+            )
+        )
+        Divider(
+            color = Color.Gray, // 선의 색상
+            thickness = 0.5.dp,   // 선의 두께
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = "We Contact",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(horizontal = 16.dp),
+            color = Color.Black
+        )
+
+        Divider(
+            color = Color.Gray, // 선의 색상
+            thickness = 0.5.dp,   // 선의 두께
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 9.dp, horizontal = 16.dp)
+        ) {
+            IconButton(
+                onClick = {},
+                modifier = Modifier
+                    .size(45.dp)
+                    .weight(1f)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_instagram),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+            IconButton(
+                onClick = {},
+                modifier = Modifier
+                    .size(45.dp)
+                    .weight(1f)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_discord),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+            IconButton(
+                onClick = {},
+                modifier = Modifier
+                    .size(45.dp)
+                    .weight(1f)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_facebook),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
         }
     }
 }
+
+
+@Composable
+fun NoticeList(noticeItems: List<String>) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .background(Color.White)
+            .clip(RoundedCornerShape(8.dp))
+    ) {
+
+        noticeItems.forEach { item ->
+            ClickableMenuItem(text = item)
+
+
+        }
+    }
+}
+
+@Composable
+fun ClickableMenuItem(text: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { /* 클릭 이벤트 처리 */ }
+            .padding(vertical = 12.dp, horizontal = 16.dp)
+    ) {
+        Text(
+            text = text,
+            fontSize = 14.sp,
+            modifier = Modifier.align(Alignment.CenterStart),
+            color = Color.Black
+        )
+    }
+}
+
+
